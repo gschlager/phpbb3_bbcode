276ad79e63817afae2049d304bac22888ff78ef3
Gerhard Schlager <mail@gerhard-schlager.at>
2018-07-03 13:31:50 +0200

    Use XML::Reader and parse B, I, U and CODE

--- lib/bbcode/xml_to_markdown.rb
+++ lib/bbcode/xml_to_markdown.rb
@@ -3,38 +3,86 @@ require 'nokogiri'
 module BBCode
   class XmlToMarkdown
     def initialize(xml)
-      @doc = Nokogiri::XML(xml)
+      @reader = Nokogiri::XML::Reader(xml)
     end
 
     def convert
       @markdown = ""
-      traverse(@doc)
+      @reader.each { |node| visit(node) }
       @markdown
     end
 
     protected
 
-    def traverse(node)
-      node.children.each { |child| visit(child) }
-    end
-
     def visit(node)
-      visitor = "visit_#{node.name}"
+      visitor = "visit_#{node.name.gsub(/\W/, '_')}"
 
       if respond_to?(visitor, include_all: true)
         send(visitor, node)
       else
-        puts visitor
-        traverse(node)
+        puts visitor unless visitor == 'visit_r'
       end
     end
 
-    def visit_text(node)
-      @markdown << node.text
+    def visit__text(node)
+      return if @ignore_node
+
+      if @within_code_block
+        @code << node.outer_xml
+      else
+        @markdown << node.outer_xml
+      end
     end
 
     def visit_B(node)
       @markdown << '**'
+    end
+
+    def visit_I(node)
+      @markdown << '_'
+    end
+
+    def visit_U(node)
+      @markdown << (start?(node) ? '[u]' : '[/u]')
+    end
+
+    def visit_CODE(node)
+      if start?(node)
+        @within_code_block = true
+        @code = ''
+      else
+        @code = CGI.unescapeHTML(@code)
+
+        if @code.include?("\n")
+          @code.sub!(/\A[\n\r]*/, '')
+          @code.rstrip!
+          @markdown = "```text\n#{@code}\n```"
+        else
+          @markdown = "`#{@code}`"
+        end
+
+        @within_code_block = false
+        @code = nil
+      end
+    end
+
+    # node for "BBCode start tag"
+    def visit_s(node)
+      @ignore_node = start?(node)
+    end
+
+    # node for "BBCode end tag"
+    def visit_e(node)
+      @ignore_node = start?(node)
+    end
+
+    # node for "ignored text"
+    def visit_i(node)
+      @ignore_node = start?(node)
+    end
+
+    def start?(node)
+      node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
     end
   end
 end
--- spec/bbcode/xml_to_markdown_spec.rb
+++ spec/bbcode/xml_to_markdown_spec.rb
@@ -15,4 +15,39 @@ RSpec.describe BBCode::XmlToMarkdown do
     xml = '<r><B><s>[b]</s>this is bold text<e>[/b]</e></B></r>'
     expect(convert(xml)).to eq('**this is bold text**')
   end
+
+  it "converts italic text" do
+    xml = '<r><I><s>[i]</s>this is italic text<e>[/i]</e></I></r>'
+    expect(convert(xml)).to eq('_this is italic text_')
+  end
+
+  it "converts underlined text" do
+    xml = '<r><U><s>[u]</s>this is underlined text<e>[/u]</e></U></r>'
+    expect(convert(xml)).to eq('[u]this is underlined text[/u]')
+  end
+
+  context "code blocks" do
+    it "converts single line code blocks" do
+      xml = '<r><CODE><s>[code]</s>one line of code<e>[/code]</e></CODE></r>'
+      expect(convert(xml)).to eq('`one line of code`')
+    end
+
+    it "converts multi line code blocks" do
+      xml = <<~XML
+        <r><CODE><s>[code]</s><i>
+        </i> /\_/\
+        ( o.o )
+         &gt; ^ &lt;
+         <e>[/code]</e></CODE></r>
+      XML
+
+      expect(convert(xml)).to eq(<<~MD.strip)
+        ```text
+         /\_/\
+        ( o.o )
+         > ^ <
+        ```
+      MD
+    end
+  end
 end
